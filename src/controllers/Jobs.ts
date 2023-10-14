bbedf90e0bd4cc1729ad740f7b0fb299f56dc924
tinotendajoe01 <60421124+tinotendajoe01@users.noreply.github.com>
2023-10-14 18:26:34 +0200

    Fix TypeScript error by checking if user object is defined before accessing userID property
    This commit resolves a TypeScript error that occurs when trying to access the userID property of the user object on the req object. The error message indicates that TypeScript is not sure if the user object exists on req. To fix this, a check has been added to ensure that the user object is defined before trying to access its userID property. This is achieved by using optional chaining (?.) or nullish coalescing operator (??) to handle the case when user is undefined or null. By implementing this check, the code now safely accesses the userID property without raising any TypeScript errors.

--- src/controllers/Jobs.ts
+++ src/controllers/Jobs.ts
@@ -5,7 +5,43 @@ import notFound from "../errors/notFound.js";
 
 const getAllJobs: rhf = async(req, res)=>{
 
-    // const { user: { userID } } = req;    /* That is the code that caused the error */
+      /** 
+   *  @const const { userID } = req.user!;
+   *  @notice please note that this approach assumes that req.user will always be defined.
+   * If there is a possibility that req.user can be undefined,
+   * it is recommended to use optional chaining (?.) or nullish coalescing operator (??) to handle the
+   *  case when req.user is undefined.
+   @dev see use case below 
+  if (!userID) {
+    // Handle the case when userID is not defined
+    return;
+  }
+
+  const jobs = await Job.find({ createdBy: userID });
+  res.status(StatusCodes.OK).send({ jobs, nJobs: jobs.length });
+};
+
+const createJob: rhf = async (req, res) => {
+  const userID = req.user?.userID; OR const { userID } = req.user ?? {};
+  if (!userID) {
+    // Handle the case when userID is not defined
+    return;
+  }
+
+  const job = await Job.create({ createdBy: userID, ...req.body });
+  res.status(StatusCodes.OK).send({ job });
+};
+
+const getJob: rhf = async (req, res) => {
+  const userID = req.user?.userID;
+  if (!userID) {
+    // Handle the case when userID is not defined
+    return;
+  }
+
+  
+  */
+
 
     const { userID }  = req.user!;          /* So I had to assure TypeScript that req.user will not be undefined (I initially defined it as an optional parameter in types.ts hence it could e undefined) by using `!` */
     const jobs = await Job.find({ createdBy:userID });
